e7eaf830ccc7ac4620699acab32c61591e48c1aa
kixv0328 <133855978+kixv0328@users.noreply.github.com>
2023-05-24 19:46:40 +0900

    sss

---
+++ Assets/LoopMap.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopMap : MonoBehaviour
+{
+    public float speed;
+
+    public SpriteRenderer sR;
+
+    public void Update()
+    {
+        Loop();
+        return;
+    }
+    
+    private void Loop()
+    {
+        Vector2 tempSize = new Vector2();
+        tempSize.x = this.sR.size.x;
+        tempSize.y = this.sR.size.y + Time.deltaTime * this.speed;
+
+        if (tempSize.y > 24f)
+        {
+            tempSize.y = 12f;
+        }
+
+        this.sR.size = tempSize;
+        return;
+    }
+}
